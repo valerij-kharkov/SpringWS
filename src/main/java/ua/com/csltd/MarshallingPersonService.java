077bc6cdef2040e9e7db135b7bc1d0d9296ff14b
valerij-kharkov <valerij.kharkov@gmail.com>
2015-09-11 18:05:27 +0300

    add test

--- src/main/java/ua/com/csltd/MarshallingPersonService.java
+++ src/main/java/ua/com/csltd/MarshallingPersonService.java
@@ -1,17 +1,38 @@
 package ua.com.csltd;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import ua.com.csltd.beans.Person;
 import ua.com.csltd.beans.PersonRequest;
 import ua.com.csltd.beans.PersonResponse;
+
+import java.math.BigInteger;
 
 /**
  * Created by valeriy_solyanik
  * on 07.09.2015.
  */
-public interface MarshallingPersonService {
-	public final static String NAMESPACE = "http://csltd.com.ua/personService/person";
+@Service
+public class MarshallingPersonService {
+	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(MarshallingPersonService.class);
 
 	/**
 	 * Gets person list.
 	 */
-	public PersonResponse getPersons(PersonRequest request);
+	public PersonResponse getPersons(PersonRequest request){
+		logger.debug("Request : {}", request);
+		PersonResponse response =  new PersonResponse();
+		response.getPerson().add(
+				new Person() {{
+					setId(BigInteger.ONE);
+					setFirstName("Joe");
+					setLastName("Smith");}});
+		response.getPerson().add(
+				new Person() {{
+					setId(BigInteger.TEN);
+					setFirstName("Joe2");
+					setLastName("Smith2");}});
+		logger.debug("Response : {}", response);
+		return response;
+	}
 }
--- src/main/java/ua/com/csltd/PersonEndpoint.java
+++ src/main/java/ua/com/csltd/PersonEndpoint.java
@@ -1,45 +1,27 @@
 package ua.com.csltd;
 
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import ua.com.csltd.beans.Person;
 import ua.com.csltd.beans.PersonRequest;
 import ua.com.csltd.beans.PersonResponse;
-
-import java.math.BigInteger;
 
 /**
  * Created by valeriy_solyanik
  * on 07.09.2015.
  */
 @Endpoint
-public class PersonEndpoint implements MarshallingPersonService {
-	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(PersonEndpoint.class);
+public class PersonEndpoint {
+	public final static String NAMESPACE = "http://csltd.com.ua/personService/person";
 
-	/**
-	 * Gets person list.
-	 */
-	@PayloadRoot(localPart="PersonRequest", namespace = NAMESPACE)
+	@Autowired
+	MarshallingPersonService service;
+
+	@PayloadRoot(localPart = "PersonRequest", namespace = NAMESPACE)
 	@ResponsePayload
 	public PersonResponse getPersons(@RequestPayload PersonRequest request) {
-		logger.debug("Request : {}", request);
-		PersonResponse response =  new PersonResponse();
-		response.getPerson().add(
-				new Person() {{
-					setId(BigInteger.ONE);
-					setFirstName("Joe");
-				setLastName("Smith");}});
-		response.getPerson().add(
-				new Person() {{
-					setId(BigInteger.TEN);
-					setFirstName("Joe2");
-					setLastName("Smith2");}});
-		logger.debug("Response : {}", response);
-		return response;
-
+		return service.getPersons(request);
 	}
-
 }
